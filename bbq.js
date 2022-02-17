af00b0eb1bd1ddb31253e4a67389cc94a7296a0b
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-02-17 13:23:37 -0800

    subscription confirmation text

---
+++ bbq.js
@@ -0,0 +1,22 @@
+const emailCollectorForm=document.getElementById("email-collector")
+const htmlContent = document.getElementById("maincontentEL")
+
+console.log(htmlContent)
+// console.log(emailCollectorForm)
+emailCollectorForm.addEventListener("submit", event => {
+    event.preventDefault()
+    console.log("submit")
+    const ourFormData = new FormData(event.target)
+    console.log(ourFormData.get("firstName"))
+
+    const userFirstName = ourFormData.get("firstName")
+
+    let updatedHTMLContent = `
+    <h2>Congratulations, ${userFirstName}!</h2>
+    
+    <p class="all-caps accent-color">You're on your way to becoming a BBQ Master!</p>
+    
+    <p class="small-text">We'll never share your information without your permission</p>
+    `
+    htmlContent.innerHTML = updatedHTMLContent
+} )
